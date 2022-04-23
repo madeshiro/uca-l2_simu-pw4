0ba874c27a84f729a48feda69db54c1f74d1fd6a
Rin Bdlt <rin.bdlt@outlook.com>
2022-04-23 20:05:30 +0200

    Add chatbox, university ToDo list (2/?)

--- webgui/www/js/script.js
+++ webgui/www/js/script.js
@@ -153,6 +153,102 @@ function loginUsingForm(formId) {
     }
 }
 
+/**
+ *
+ * @param visible {boolean}
+ */
+function showChatbox(visible) {
+    let chatboxButton = document.getElementById('chatbox-display-button');
+    let chatboxHtml = document.getElementById('chatbox');
+
+    chatboxButton.style.visibility = visible ? 'hidden' : 'visible';
+    chatboxHtml.style.visibility = visible ? 'visible' : 'hidden';
+
+    if (visible) {
+        refreshChatbox();
+    }
+}
+
+function refreshChatbox() {
+    let refreshRqst = new XMLHttpRequest()
+    refreshRqst.open('GET', '/htbin/chatget.py', true);
+    refreshRqst.onload = function () {
+        let responseJSON = JSON.parse(refreshRqst.responseText);
+        let chatboxMsgHtml = document.getElementById('chatbox-messages');
+        chatboxMsgHtml.innerHTML = ''; // clear all elements
+
+        if (responseJSON) {
+            for (let msg of responseJSON) {
+                addMsgChatbox(msg.user, msg.msg, `${msg.date} ${msg.time}`);
+            }
+        }
+    }
+    refreshRqst.send();
+}
+
+/**
+ *
+ * @param inputElem {HTMLInputElement}
+ */
+function sendMsgChatbox(inputElem) {
+    let msg = inputElem.value;
+    if (msg !== '') {
+        let ajaxRequest = new XMLHttpRequest();
+        ajaxRequest.open('GET',`/htbin/chatsend.py?msg=${msg}`);
+        ajaxRequest.onload = function () {
+            let response = JSON.parse(ajaxRequest.responseText);
+            switch (response.num) {
+                case 0:
+                    refreshChatbox();
+                    break;
+                case 1:
+                    addMsgChatbox('ChatBox', 'Cannot send empty message');
+                    break;
+                case -1:
+                    addMsgChatbox('ChatBox', 'An error occured while sending the message. Please contact an admin to report it.');
+                    break;
+            }
+            inputElem.value = '';
+        }
+        ajaxRequest.send();
+    }
+}
+
+/**
+ *
+ * @param user  {string}
+ * @param msg   {string}
+ * @param date  {string|null}
+ */
+function addMsgChatbox(user, msg, date= null) {
+    if (date === null) {
+        let today = new Date();
+        date = `${today.getDay().toFixed(2)}/${today.getMonth().toFixed(2)}/${today.getFullYear()} `;
+        date += `${today.getHours().toFixed(2)}:${today.getMinutes().toFixed(2)}`;
+    }
+    
+    let msgsHtml = document.getElementById('chatbox-messages');
+    let msgHtml  = document.createElement('div');
+    msgHtml.classList.add('chatbox-message');
+    let msgInfoHtml = document.createElement('div');
+    msgInfoHtml.classList.add('chatbox-msginfo');
+    let aUser = document.createElement('a');
+    let aDate = document.createElement('a');
+    let pText = document.createElement('p');
+    pText.classList.add('chatbox-msgcontent');
+
+    aUser.innerText = user;
+    aDate.innerText = date;
+    pText.innerText = msg;
+
+    msgInfoHtml.appendChild(aUser);
+    msgInfoHtml.appendChild(aDate);
+    msgHtml.appendChild(msgInfoHtml);
+    msgHtml.appendChild(pText);
+    msgsHtml.appendChild(msgHtml);
+    msgsHtml.scrollTo(0, msgsHtml.scrollHeight)
+}
+
 window.onload = function () {
     detectColorScheme();
     document.documentElement.dataset.scroll = window.scrollY;
@@ -165,6 +261,12 @@ window.onload = function () {
 
         document.getElementById("cpp-apps-button")
             .children[0].href += `?port=${port}`;
+    }
+
+    document.getElementById('chatbox-send-text').onkeyup = function(e) {
+        if (e.keyCode === 13) {
+            sendMsgChatbox(document.getElementById('chatbox-send-text'));
+        }
     }
 }
 
